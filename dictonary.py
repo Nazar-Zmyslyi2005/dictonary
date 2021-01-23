d626742345792f59aae9e7e14dbd3df25c7ffd53
Nazar-Zmyslyi2005 <77897488+Nazar-Zmyslyi2005@users.noreply.github.com>
2021-01-23 23:23:03 +0200

    Add files via upload

---
+++ dictonary.py
@@ -0,0 +1,16 @@
+f_num = float(imput("Виберіть перше число"))
+oper = input("Виберіть дію")
+s_num = float(input("Виберіть друге число"))
+prodolzhit = 'y'
+while prodolzhit = 'y':
+    if oper == '+':
+        print(f_num + s_num)
+    elif oper == '-':
+        print(f_num - s_num)
+    elif oper == '*':
+        print(f_num * s_num)
+    elif oper == '/':
+        print(f_num / s_num)
+    else:
+        print("Error")
+    prodolzhit = input(Ввдіть 'y', щоб  продовжити, або любу кнопку, щоб звершити)
